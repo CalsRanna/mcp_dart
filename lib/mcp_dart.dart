d1247a3684d790c65dc025a6f8603e73a3cb60cd
Cals <cals@CalsdeMac-mini.local>
2025-03-20 13:46:08 +0800

    feat: add tool export and reorder server_config export

--- lib/mcp_dart.dart
+++ lib/mcp_dart.dart
@@ -1,4 +1,5 @@
 export 'src/client/sse_client.dart';
 export 'src/client/stdio_client.dart';
-export 'src/server/server_config.dart';
 export 'src/message.dart';
+export 'src/server/server_config.dart';
+export 'src/tool.dart';
